08188a56197253ee29a22a380e9165106c2a24fd
Rminoro <rminoro@outlook.com>
2022-12-05 22:21:53 -0300

    Exerc5-Lista5-funcoes
    Exercício 5 lista

---
+++ EXERC5/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner S = new Scanner(System.in);
+
+        System.out.println("Digite a taxa de imposto: ");
+        int taxaImposto = S.nextInt();
+
+        System.out.println("Digite o custo: ");
+        int custo = S.nextInt();
+        public static void somaImposto(double taxaImposto *0.01, custo);
+
+
+    }
+
+ 
+    
+}
